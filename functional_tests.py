24ab61115c5e70c650629cce4e1adb7fcce12c73
Venky Gangisetti <venkateshgangisetti@outlook.com>
2024-12-21 16:15:38 -0600

    Functinal test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,8 @@
 import unittest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
@@ -9,9 +12,23 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_start_a_todo_list(self):
         self.browser.get("http://localhost:8000")
-        self.assertIn("To-Do", self.browser.title)
 
-        self.fail("Finish the test")
+        self.assertIn("To-Do", self.browser.title)
+        header_text = self.browser.find_element(By.TAG_NAME, "h1").text
+        self.assertIn("To-Do", header_text)
+
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        self.assertEqual(inputbox.get_attribute("placeholder"), "Enter a to-do item")
+
+        inputbox.send_keys("Buy peacock feathers")
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element(By.ID, "id_list_table")
+        rows =  table.find_elements(By.TAG_NAME, "tr")
+        self.assertTrue(any(row.text == "1: Buy peacock feathers" for row in rows))
+
+        self.fail("Finish the test!")
 
 
 if __name__ == "__main__":
