8358c7ead9f5fa2b32d588659186ff14bd16b26b
Venky Gangisetti <venkateshgangisetti@outlook.com>
2024-12-21 15:00:17 -0600

    First FT spaced out in comments, and now uses unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,18 @@
+import unittest
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get("http://localhost:8000")
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+    def tearDown(self):
+        self.browser.quit()
 
-assert "Congratulations!" in browser.title
-print("Ok")
+    def test_can_start_a_todo_list(self):
+        self.browser.get("http://localhost:8000")
+        self.assertIn("To-Do", self.browser.title)
+
+        self.fail("Finish the test")
+
+
+if __name__ == "__main__":
+    unittest.main()
